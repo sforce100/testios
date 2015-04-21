bbc64e123a2df2494fca2b9d3c42a1ae3556d603
hzh <sforce1000@gmail.com>
2015-04-21 19:24:56 +0800

    Initial Commit

---
+++ Attribute/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Attribute
+//
+//  Created by hzh on 15/4/21.
+//  Copyright (c) 2015年 LittleFox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
