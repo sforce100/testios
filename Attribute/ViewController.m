e32d14903a24af929e325604c8638889dc185f53
hzh <sforce1000@gmail.com>
2015-04-22 15:26:56 +0800

    init

--- Attribute/ViewController.m
+++ Attribute/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "DetailViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *body;
+@property (weak, nonatomic) IBOutlet UIButton *outlineButton;
 
 @end
 
@@ -17,11 +20,69 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSMutableAttributedString *title = [[NSMutableAttributedString alloc] initWithString:self.outlineButton.currentTitle];
+    [title setAttributes:@{NSStrokeWidthAttributeName : @3,
+                           NSStrokeColorAttributeName : self.outlineButton.tintColor}
+                   range:NSMakeRange(0, title.length)];
+    [self.outlineButton setAttributedTitle:title forState:UIControlStateNormal];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self usePerferredFonts];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(perferredFontsChange)
+                                                 name:UIContentSizeCategoryDidChangeNotification
+                                               object:nil];
+}
+
+-(void) viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIContentSizeCategoryDidChangeNotification
+                                                  object:nil];
+}
+
+-(void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if ([segue.identifier isEqualToString:@"HomeDetail"]) {
+        if ([segue.destinationViewController isKindOfClass:[DetailViewController class]]) {                  DetailViewController *detail = (DetailViewController *)segue.destinationViewController;
+            detail.textContent = @"detail from home";
+        }
+    }
+}
+
+-(BOOL) shouldPerformSegueWithIdentifier:(NSString *)identifier sender:(id)sender {
+    if ([identifier isEqualToString:@"isNotHomeDetail"]) {
+        return NO;
+    }
+    return YES;
+}
+
+-(void) perferredFontsChange:(NSNotification *)notification
+{
+    [self usePerferredFonts];
+}
+
+
+-(void) usePerferredFonts
+{
+    self.body.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+}
+
+- (IBAction)changeSelectTextColorWithBackgroundColor:(UIButton *)sender
+{
+    [self.body.textStorage addAttribute:NSForegroundColorAttributeName
+                                  value:sender.backgroundColor
+                                  range:self.body.selectedRange];
+}
+- (IBAction)outlineBodySelect:(id)sender {
+    [self.body.textStorage addAttributes:@{ NSStrokeWidthAttributeName : @-3, NSStrokeColorAttributeName : [UIColor blackColor]}
+                                   range:self.body.selectedRange];
 }
 
 @end
