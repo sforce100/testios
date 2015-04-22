60c6d3c9b1346bcec9d98d2f4ccee13ae5e33572
hzh <sforce1000@gmail.com>
2015-04-22 15:29:35 +0800

    init

---
+++ Attribute/Base.lproj/DetailViewController.h
@@ -0,0 +1,15 @@
+//
+//  DetailViewController.h
+//  Attribute
+//
+//  Created by hzh on 15/4/22.
+//  Copyright (c) 2015年 LittleFox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property(nonatomic, strong) NSAttributedString *textContent;
+
+@end
---
+++ DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  
+//
+//  Created by hzh on 15/4/22.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
